fc0a3c9e235fdecbee7f394c01fac5ed441856b8
2026afk <darwinpalomino17@gmail.com>
2026-09-21 20:28:30 -0500

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// Este archivo conecta las tres páginas del sitio:
+// elegir-cuenta.html, registro.html y principal.html
+
+// Lleva al usuario a la página principal (después de elegir el perfil)
+function irAPrincipal() {
+  window.location.href = "principal.html";
+}
+
+// Lleva al usuario a la página de registro (crear perfil nuevo)
+function irARegistro() {
+  window.location.href = "registro.html";
+}
+
+// Lleva al usuario de vuelta a la página de elegir cuenta
+function irAIndex() {
+  window.location.href = "index.html";
+}
